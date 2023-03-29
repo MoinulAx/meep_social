b90ca5defc3cd3954d237a63c2f78b7c4dc446bf
MoinulAx <mkhan@brooklynsteamcenter.org>
2023-03-29 10:55:13 -0400

    fdsdfs

--- main.py
+++ main.py
@@ -41,6 +41,24 @@ def post_feed():
     results= cursor.fetchall()
     return render_template("feed.html.jinja", posts=results)
 
+@app.route('/sign-in')
+def sign_in(): 
+    return render_template("sign_in.html.jinja")
+
+@app.route('/sign-up',methods=['POST','GET'])
+def sign_up():
+    if request.method == 'POST':
+    
+        cursor=connection.cursor()
+        cursor.execute("""
+            INSERT INTO `users` (`user`,`password`,`Email`,`Bio`,`display_name`,`avatar`)
+            VALUES (%s,%s,%s,%s,%s,%s)
+        """,())
+        return request.form
+    
+    elif request.method == 'GET':
+        return render_template("sign_up.html.jinja")    
+
 
 if __name__=='__main__':
     app.run(debug=True)
