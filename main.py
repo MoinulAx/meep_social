8e5adf77eb3d610b36f4ad31d733b281e2b5bb27
MoinulAx <mkhan@brooklynsteamcenter.org>
2023-03-30 10:51:24 -0400

    jnjn

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ import pymysql.cursors
 import pymysql
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager
+
+login_manager= LoginManager
+
 
 app= Flask(__name__)
 auth = HTTPBasicAuth()
+
+login_manager.init_app(app)
+
+class User:
+    def __init__(self,id,user,banned):
+        self.is_authenticated = True
+        self.is_anonymous= False
+        self.is_active = not banned
+
+        self.username=user
+        self.id=id
+    
+    def get_id(self):
+        return str(self.id)
 
 users = {
     "mkhan": generate_password_hash("1234"),
@@ -29,6 +47,19 @@ connection= pymysql.connect(
 
 )
 
+@login_manager.user_loader
+def user_loader(user_id):
+    cursor= connection.cursor()
+
+    cursor.execute("SELECT * from `users` WHERE `id` = "+user_id)
+
+    result = cursor.fetchone()
+
+    if result is None:
+        return None
+    
+    return User(result['id'],result['user'],result['banned'],)
+
 @app.route("/")
 def index(): 
     return render_template("meeptem.html.jinja")
@@ -50,11 +81,21 @@ def sign_up():
     if request.method == 'POST':
     
         cursor=connection.cursor()
+
+        Avatar = request.files['Avatar']
+        file_name=Avatar.filename
+        file_extension=file_name.split('.')[-1]
+        if file_extension in ['jpg','jpeg','png','gif']:
+            Avatar.save('media/users/' +file_name)
+        else:
+            raise Exception('Invaild file type')
+
+
         cursor.execute("""
             INSERT INTO `users` (`user`,`password`,`Email`,`Bio`,`display_name`,`avatar`)
             VALUES (%s,%s,%s,%s,%s,%s)
-        """,())
-        return request.form
+        """,(request.form['username'],request.form['password'],request.form['email'],request.form['bio'],request.form['displayname'],file_name))
+        return redirect("/feed")
     
     elif request.method == 'GET':
         return render_template("sign_up.html.jinja")    
